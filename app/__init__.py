29298ed7df76817cd57e7fd833d3d8dffab87337
Eze - Oge Samuella <ezeogesamuella@gmail.com>
2026-09-14 11:51:14 +0100

    Add Flask application factory

---
+++ app/__init__.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+from config import Config
+
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = "auth.login"
+login_manager.login_message_category = "warning"
+
+
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    from app.auth.routes import auth_bp
+    from app.main.routes import main_bp
+
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(main_bp)
+
+    with app.app_context():
+        db.create_all()
+
+    return app
